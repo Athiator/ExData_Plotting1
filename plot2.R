64231bc75d63057ba55688d71cbc2a988c1f975e
Athiator <athiator@gmail.com>
2014-05-11 18:14:41 +1000

    Plots 1-3
    commit of completed scripts and plots 1-3 and shell for plot 4

---
+++ plot2.R
@@ -0,0 +1,24 @@
+
+## Read in the data. This method inspired by a post on the class forums by Cory Brunson
+
+## Identify the lines for the correct dates
+lines <-grep('^[1-2]/2/2007',readLines('./household_power_consumption.txt'))
+skipRows <- lines[1] - 1
+
+powerData <- read.table("./household_power_consumption.txt", sep = ";", skip = skipRows, na.strings = "?", nrows = length(lines))
+
+## That data frame doesn't pick up the header, so, read the first couple of lines and copy the column names across:
+
+powerHeader <- read.table("./household_power_consumption.txt", sep = ";",header = TRUE, nrows = 1)
+
+colnames(powerData)<-colnames(powerHeader)
+
+## Set the date and time columns to be objects of those classes.
+## powerData$Date <- as.Date(powerData$Date, format = "%d/%m/%Y")
+
+powerData <- transform(powerData, TrueTime = strptime(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S"))
+
+## Generate plot 2
+png(file = "./plot2.png", width = 480, height = 480)
+plot(powerData$TrueTime, powerData$Global_active_power, type = 'l', xlab = "", ylab = "Global Active Power (kilowatts)", main = "")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,27 @@
+
+## Read in the data. This method inspired by a post on the class forums by Cory Brunson
+
+## Identify the lines for the correct dates
+lines <-grep('^[1-2]/2/2007',readLines('./household_power_consumption.txt'))
+skipRows <- lines[1] - 1
+
+powerData <- read.table("./household_power_consumption.txt", sep = ";", skip = skipRows, na.strings = "?", nrows = length(lines))
+
+## That data frame doesn't pick up the header, so, read the first couple of lines and copy the column names across:
+
+powerHeader <- read.table("./household_power_consumption.txt", sep = ";",header = TRUE, nrows = 1)
+
+colnames(powerData)<-colnames(powerHeader)
+
+## Set the date and time columns to be objects of those classes.
+## powerData$Date <- as.Date(powerData$Date, format = "%d/%m/%Y")
+
+powerData <- transform(powerData, TrueTime = strptime(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S"))
+
+## Generate plot 3
+png(file = "./plot3.png", width = 480, height = 480)
+plot(powerData$TrueTime, powerData$Sub_metering_1, type = 'l', xlab = "", ylab = "Energy sub metering", main = "")
+lines(powerData$TrueTime, powerData$Sub_metering_2, col = "red")
+lines(powerData$TrueTime, powerData$Sub_metering_3, col = "blue")
+legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = 1)
+dev.off()
